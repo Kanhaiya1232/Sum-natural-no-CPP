ef1084cb30a6feeff3ba381e75157a9c6956b74f
Kanhaiya1232 <101667615+Kanhaiya1232@users.noreply.github.com>
2022-04-07 18:56:26 +0530

    Add files via upload

---
+++ sum natural no.cpp
@@ -0,0 +1,14 @@
+//sum of natural no
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,sum=0;
+for(i=1;i<=10;i++)
+{
+    cout<< i<<endl;
+ sum=sum+i;
+}
+cout<<"Sum of Natural Number"<<sum;
+return 0;
+}
